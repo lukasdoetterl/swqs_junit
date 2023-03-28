e558b04a673812a6db333ea905e1575b2a5e3f94
lu741doe <lukas.doetterl@htwg-konstanz.de>
2023-03-28 12:28:56 +0200

    created test class for Trimmer

--- src/test/java/com/example/project/TrimmerTests.java
+++ src/test/java/com/example/project/TrimmerTests.java
@@ -1,6 +1,67 @@
 package com.example.project;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TrimmerTests {
-    //test 1
-    //test 2
+
+        @Test
+        void testTrimLeft() {
+            //init
+            Trimmer trimmer = new Trimmer();
+            //run
+            String result = trimmer.trim("   Test", ' ');
+            //assert
+            assertEquals("Test", result);
+            //cleanup
+        }
+
+        //test that it trims a string on the right side
+        @Test
+        void testTrimRight() {
+            //init
+            Trimmer trimmer = new Trimmer();
+            //run
+            String result = trimmer.trim("Test   ", ' ');
+            //assert
+            assertEquals("Test", result);
+            //cleanup
+        }
+
+        //test that it trims a string on both sides
+        @Test
+        void testTrimBoth() {
+            Trimmer trimmer = new Trimmer();
+            String result = trimmer.trim("   Test   ", ' ');
+            assertEquals("Test", result);
+        }
+
+        //test that it doesn't trim a string that doesn't need trimming
+        @Test
+        void testTrimNoTrim() {
+            //init
+            Trimmer trimmer = new Trimmer();
+            //run
+            String result = trimmer.trim("Test", ' ');
+            //assert
+            assertEquals("Test", result);
+            //cleanup
+        }
+    @Test
+    void testExtremeLongTrim() {
+        //init
+        Trimmer trimmer = new Trimmer();
+        //run
+        String result = trimmer.trim("                                                                                                                             Test                                                                         ", ' ');
+        //assert
+        assertEquals("Test", result);
+        //cleanup
+    }
+
+
+
+
+
+
 }
