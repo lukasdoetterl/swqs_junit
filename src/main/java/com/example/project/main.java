023580ac2258a0c5658d77f645c49baca61e1488
lukasdoetterl <lukasdoetterl@hotmail.com>
2023-04-03 14:14:39 +0200

    made a main to use the programm

---
+++ src/main/java/com/example/project/main.java
@@ -0,0 +1,38 @@
+package com.example.project;
+
+import java.util.Scanner;
+
+public class main {
+    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+    Morse morse = new Morse("");
+public void checkInput() {
+
+
+    System.out.println("Enter the text you want to code");
+
+    String userinput = scanner.nextLine();  // Read user input
+    System.out.println("The Input is as follow " + userinput);  // Output user input
+
+
+    String result = morse.encodeToMorse(userinput);
+    System.out.println("The Morse code is as follow " + result);  // Output user input
+
+
+
+}
+        public static void main(String[] args) {
+            main main = new main();
+            while (true){
+                main.checkInput();
+            }
+
+
+
+
+
+
+
+
+
+        }
+}
