fc1194d3e054b3cd9906bdc6839b94865d5c1d09
lukasdoetterl <lukasdoetterl@hotmail.com>
2023-05-22 12:17:14 +0200

    test

--- src/test/java/com/example/project/TrimmerTests.java
+++ src/test/java/com/example/project/TrimmerTests.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TrimmerTests {
-//test
-    //test2
+
         @Test
         void testTrimLeft() {
             //init
